63b39f39d7377b305726df542869b2d458dfbe42
dylanalbios <dylxanj430@gmail.com>
2023-06-10 14:00:46 -0700

    added getOneDish function and renamed list function

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -9,12 +9,25 @@ const nextId = require("../utils/nextId");
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
 
-// list all dishes
-function list(req, res) {
+// get all dishes
+function getAllDishes(req, res, next) {
     res.json({ data: dishes });
-}
+};
 
+// get one dish based on id
+function getOneDish(req, res) {
+    const { dishId } = req.params;
+
+    const foundDish = dishes.find((dish) => dish.id === Number(dishId));
+
+    if (foundDish) {
+        res.json({ data: foundDish });
+    }   else {
+        res.status(404).json({ error: `Dish id not found: ${dishId}`});
+    }
+};
 
 module.exports ={
-    list,
+    getAllDishes,
+    getOneDish,
 }
