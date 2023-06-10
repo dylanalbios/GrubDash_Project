5502a70bf999450b42143710365ed6ad8e0a0f33
dylanalbios <dylxanj430@gmail.com>
2023-06-09 20:31:25 -0700

    basic structure and routing added

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,24 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
+router
+    .route("/")
+    .get()
+    .post()
+    .put()
+    .delete()
+    .all(methodNotAllowed);
+
+router
+    .route("/:orderId")
+    .get()
+    .post()
+    .put()
+    .delete()
+    .all(methodNotAllowed);
+
 
 module.exports = router;
