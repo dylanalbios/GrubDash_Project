097bc248c5011061852cce3309e1fc8d76fb9a47
dylanalbios <dylxanj430@gmail.com>
2023-06-10 14:54:30 -0700

    list, orderExists, and read functon added and exported

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,44 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+
+// list all orders or filter id
+function list(req, res) {
+    const { orderId } = req.params;
+
+    if (orderId) {
+        const foundOrder = orders.find((order) => order.id === Number(orderId));
+
+        if (foundOrder) {
+            res.json({ data: foundOrder });
+        } else {
+            res.status(404).json({ error: `Order id not found: ${orderId}` });
+        }
+    } else {
+        res.json({ data: orders });
+    }
+};
+
+// verify order exists
+function orderExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+    if (foundOrder) {
+        res.locals.order = foundOrder;
+        return next();
+    }
+    return res.status(404).json({ error: `Order id not found: ${orderId}` });
+};
+
+// read orders
+function read(req, res, next) {
+    res.json({ data: res.locals.order });
+};
+
+
+
+module.exports ={
+    list,
+    read: [orderExists, read],
+}
