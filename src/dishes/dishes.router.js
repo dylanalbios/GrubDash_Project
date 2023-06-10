0fd32d2c3f7fd9abc06245e549d3219181a7fac1
dylanalbios <dylxanj430@gmail.com>
2023-06-09 20:29:28 -0700

    basic structure and routing added

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,4 +1,6 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 
